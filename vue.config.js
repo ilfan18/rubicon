b8af7f3f59e8498ea4aeb171680bc8c821cfec03
ilfan <ilfanmuratov@gmail.com>
2022-06-09 17:16:43 +0500

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/rubicon/',
+	outputDir:
+		process.env.NODE_ENV === 'production'
+			? __dirname + '/lib'
+			: __dirname + '/demo',
+	css: {
+		extract: false,
+	},
+});
