fa0399e24bc7eaa00e82b762eb67c8dfb5363735
ilfan <ilfanmuratov@gmail.com>
2022-06-18 13:35:32 +0500

    Starting global styles

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/rubicon/',
@@ -8,5 +9,11 @@ module.exports = defineConfig({
 			: __dirname + '/demo',
 	css: {
 		extract: false,
+	},
+	pluginOptions: {
+		'style-resources-loader': {
+			preProcessor: 'scss',
+			patterns: [path.resolve(__dirname, './src/styles/*.scss')],
+		},
 	},
 });
